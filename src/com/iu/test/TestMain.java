2af6f12e41f6bed0d2d24270ab8894350957fd28
simyeji <skeosk424@gmail.com>
2022-07-27 12:23:55 +0900

    testend

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -6,14 +6,13 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		
-		DBConnector dbc = new DBConnector();
-		
 		try {
-			dbc.getConnection();
+			DBConnector.getConnection();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+
 	}//메인 메서드 종료
 }//클래스 종료
